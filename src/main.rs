aa3ff7eeaafec4c15f77324fad486ae4869d6b67
August Dahlkvist <AugustDahlkvist@gmail.com>
2025-01-26 13:56:44 +0100

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,80 @@
-use std::collections::HashMap;
+#![allow(dead_code)]
+#![allow(unused)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+#![allow(unused_import_braces)]
 
-#[allow(dead_code)]
-#[allow(unused)]
-#[allow(unused_variables)]
-#[allow(unused_mut)]
-
+use std::{collections::HashMap, time::Duration};
 use bevy::prelude::*;
+use bevy::text::FontSmoothing;
+use bevy::{input::keyboard::Key, time::Stopwatch};
+use rand::Rng;
 
-#[derive(Component)]
-enum Direction
+// =============================================
+// GAMESTATE
+// =============================================
+#[derive(Eq, PartialEq, Clone, Copy, Debug)]
+enum GameState
 {
-    Up,
-    Down,
-    Left,
-    Right,
-    None
+    Begin,
+    Round,
+    GameOver
 }
+
+#[derive(Resource)]
+struct GameManager
+{
+    time_since_round_start : Stopwatch,
+    curr_state: GameState
+}
+
+impl GameManager
+{
+    pub fn new() -> Self
+    {
+        Self
+        {
+            time_since_round_start: Stopwatch::new(),
+            curr_state: GameState::Begin
+        }
+    }
+
+    fn start_round(&mut self)
+    {
+        self.curr_state = GameState::Round;
+        self.time_since_round_start.reset();
+    }
+}
+
+impl Default for GameManager
+{
+    fn default() -> Self
+    {
+        return GameManager::new();
+    }
+}
+
+fn update_gamemanager(time: Res<Time>, mut manager: ResMut<GameManager>)
+{
+    match manager.curr_state 
+    {
+        GameState::Begin =>
+        {
+
+        }
+        GameState::Round =>
+        {
+            manager.time_since_round_start.tick(time.delta());
+        }
+        GameState::GameOver =>
+        {
+
+        }
+    }
+    
+}
+
 
 // =============================================
 // ANIMATION
@@ -132,7 +191,111 @@ fn animate_sprite(time: Res<Time>, mut query: Query<(&mut SpriteAnimator, &mut S
 }
 
 
+// =============================================
+// MOLE
+// =============================================
+const MOLE_HIDE_ANIM: &str = "MoleHide";
+const MOLE_RISE_ANIM: &str = "MoleUp";
+const MOLE_BONK_ANIM: &str = "MoleBonk";
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum MoleState
+{
+    Hidden,
+    HeadUp,
+    Bonked
+}
+
+#[derive(Component)]
+struct Mole
+{
+    kill_key: KeyCode,
+    status: MoleState,
+    timer: Timer
+}
+
+impl Mole
+{
+    fn new(key: KeyCode) -> Self
+    {
+        let mut new_mole = Self
+        {
+            kill_key: key,
+            status: MoleState::Hidden,
+            timer: Timer::new(Duration::from_secs(1), TimerMode::Repeating)
+        };
+
+        new_mole.reset_mole_time();
+
+        return new_mole;
+    }
+
+    fn reset_mole_time(&mut self)
+    {
+        let mut rng = rand::thread_rng();
+        let duration = match self.status
+        {
+            MoleState::Hidden => rng.gen_range(4.0 .. 14.0),
+            MoleState::HeadUp => rng.gen_range(2.0 .. 3.0),
+            MoleState::Bonked => rng.gen_range(1.0 .. 2.0),
+        };
+
+        self.timer.set_duration(Duration::from_secs_f32(duration));
+    }
+}
+
+fn update_moles(time: Res<Time>, 
+    keys: Res<ButtonInput<KeyCode>>,
+    manager: Res<GameManager>,
+    mut query: Query<(&mut SpriteAnimator, &mut Mole)>)
+{
+    let elapsed_sec = manager.time_since_round_start.elapsed_secs();
+    let max_mole_up = (1.0 - (1.0 / (1.0 + elapsed_sec * 0.01))) * 18.0;
+    let total_mole_up = query.iter().filter(|(a, m)| m.status == MoleState::HeadUp).count() as f32;
+
+    for (mut animator, mut mole) in &mut query
+    {
+        mole.timer.tick(time.delta());
+        let prev_state = mole.status;
+
+        if keys.just_pressed(mole.kill_key) && mole.status == MoleState::HeadUp
+        {
+            animator.play_anim(MOLE_BONK_ANIM);
+            mole.status = MoleState::Bonked;
+        }
+        else if mole.timer.just_finished()
+        {
+            mole.status = match mole.status 
+            {
+                MoleState::Hidden => if total_mole_up < max_mole_up { MoleState::HeadUp } else { MoleState::Hidden },
+                MoleState::HeadUp => MoleState::Hidden,
+                MoleState::Bonked => MoleState::Hidden,
+            };
+
+            // Do random something here.
+            mole.reset_mole_time();
+
+            // Adjust time to make game increasingly difficult.
+            let new_dur = mole.timer.duration();
+            let diff_factor = 1.0 / (elapsed_sec * 0.02 + 2.0) + 0.5;
+            mole.timer.set_duration(Duration::from_secs_f32(new_dur.as_secs_f32() * diff_factor));
+        }
+
+        if prev_state != mole.status && prev_state != MoleState::Bonked
+        {
+            match mole.status 
+            {
+                MoleState::HeadUp => animator.play_anim(MOLE_RISE_ANIM),
+                MoleState::Hidden => animator.play_anim(MOLE_HIDE_ANIM),
+                MoleState::Bonked => animator.play_anim(MOLE_BONK_ANIM),
+            }
+        }
+    }
+}
+
+// =============================================
+// MAIN
+// =============================================
 fn main()
 {
     App::new()
@@ -140,13 +303,15 @@ fn main()
         .add_plugins((DefaultPlugins.set(ImagePlugin::default_nearest()), bevy_framepace::FramepacePlugin))
 
         // Startup
+        .init_resource::<GameManager>()
         .add_systems(Startup, 
             setup)
         
         // Update
         .add_systems(Update, 
-            (sprite_movement,
-                    animate_sprite))
+            (update_gamemanager,
+                    animate_sprite,
+                    update_moles))
 
         .run();
 }
@@ -158,43 +323,10 @@ fn setup(mut commands: Commands,
     mut settings: ResMut<bevy_framepace::FramepaceSettings>,
     mut window: Single<&mut Window>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-    asset_server: Res<AssetServer>)
+    asset_server: Res<AssetServer>,
+    mut game_manager: ResMut<GameManager>)
 {
-    commands.spawn(Camera2d::default());
-    
-    // Game BG
-    commands.spawn(Sprite::from_image(asset_server.load("GameBG.png")));
-
-    // Setup moles
-    let texture = asset_server.load("Mole.png");
-    let layout = TextureAtlasLayout::from_grid(UVec2::new(46, 37), 2, 2, Some(UVec2::new(1, 1)), None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-
-    let mut mole_start = Transform::from_scale(Vec3::splat(2.0));
-    mole_start.translation = Vec3::new(100.0, 0.0, 1.0);
-
-    let mut anim_controller = SpriteAnimator::new();
-    anim_controller.push_anim("MoleUp", SpriteAnimation::new( &vec![3,2,1,0], 0.8));
-    anim_controller.play_anim("MoleUp");
-
-    commands.spawn((
-        Sprite::from_atlas_image(
-            texture,
-            TextureAtlas {
-                layout: texture_atlas_layout,
-                index: 0,
-            },
-        ),
-        mole_start,
-        anim_controller
-    ));
-
-    // commands.spawn(
-    //     (Sprite::from_image(asset_server.load("Arnold.png")),
-    //     Transform::from_xyz(100., 0., 1.),
-    //     Direction::Up)
-    // );
-
+    // Setup window
     use bevy_framepace::Limiter;
     settings.limiter = Limiter::from_framerate(60.0);
 
@@ -202,42 +334,174 @@ fn setup(mut commands: Commands,
     window.resizable = false;
     window.name = Some(String::from("Thor's Power"));
     window.title = String::from("Thor's Power");
+
+    commands.spawn(Camera2d::default());
+    
+    // Game BG
+    commands.spawn(Sprite::from_image(asset_server.load("GameBG.png")));
+    
+
+    // Create moles
+    create_all_moles(&mut commands, &mut texture_atlas_layouts, &asset_server);
+
+    game_manager.start_round();
+}
+
+fn create_all_moles(commands: &mut Commands, texture_atlas_layouts: &mut ResMut<Assets<TextureAtlasLayout>>, asset_server: &Res<AssetServer>)
+{
+    // Setup moles
+    let texture = asset_server.load("Mole.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::new(46, 37), 3, 2, Some(UVec2::new(1, 1)), None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    let font = asset_server.load("Pixica-Bold.ttf");
+    let text_font = TextFont {
+        font: font.clone(),
+        font_size: 50.0,
+        ..default()
+    };
+
+    // Top row
+    create_mole_at(commands, Vec2::new(-460.0, -38.0) ,  KeyCode::KeyQ, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-364.0, -38.0) ,  KeyCode::KeyW, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-268.0, -38.0) ,  KeyCode::KeyE, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-172.0, -38.0) ,  KeyCode::KeyR, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-76.0, -38.0) ,  KeyCode::KeyT, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(20.0, -38.0) ,  KeyCode::KeyY, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(116.0, -38.0) ,  KeyCode::KeyU, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(212.0, -38.0) ,  KeyCode::KeyI, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(308.0, -38.0) ,  KeyCode::KeyO, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(404.0, -38.0), KeyCode::KeyP, &texture, &texture_atlas_layout, &font, &text_font);
+
+    // Middle row
+    create_mole_at(commands, Vec2::new(-424.0, -112.0),  KeyCode::KeyA, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-328.0, -112.0),  KeyCode::KeyS, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-232.0, -112.0),  KeyCode::KeyD, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-136.0, -112.0),  KeyCode::KeyF, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-40.0, -112.0),  KeyCode::KeyG, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(56.0, -112.0),  KeyCode::KeyH, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(152.0, -112.0),  KeyCode::KeyJ, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(248.0, -112.0),  KeyCode::KeyK, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(344.0, -112.0),  KeyCode::KeyL, &texture, &texture_atlas_layout, &font, &text_font);
+
+    // Bottom row
+    create_mole_at(commands, Vec2::new(-358.0, -186.0),  KeyCode::KeyZ, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-262.0, -186.0),  KeyCode::KeyX, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-166.0, -186.0),  KeyCode::KeyC, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(-70.0, -186.0),  KeyCode::KeyV, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(26.0, -186.0),  KeyCode::KeyB, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(122.0, -186.0),  KeyCode::KeyN, &texture, &texture_atlas_layout, &font, &text_font);
+    create_mole_at(commands, Vec2::new(218.0, -186.0),  KeyCode::KeyM, &texture, &texture_atlas_layout, &font, &text_font);
+}
+
+fn create_mole_at(commands: &mut Commands, mut pos: Vec2, key: KeyCode, texture: &Handle<Image>,  texture_atlas_layout: &Handle<TextureAtlasLayout>, font: &Handle<Font>, text_font: &TextFont)
+{
+    pos += Vec2::new(46.0, -35.0);
+    let mut mole_start = Transform::from_scale(Vec3::splat(2.0));
+    mole_start.translation = Vec3::new(pos.x, pos.y, 1.0);
+
+    let mut anim_controller = SpriteAnimator::new();
+
+    anim_controller.push_anim(MOLE_RISE_ANIM, SpriteAnimation::new( &vec![4,3,1,0], 0.08));
+    anim_controller.push_anim(MOLE_HIDE_ANIM, SpriteAnimation::new( &vec![0,1,3,4], 0.04));
+    anim_controller.push_anim(MOLE_BONK_ANIM, SpriteAnimation::new( &vec![2,4,2,4,2,4,2,4], 0.05));
+
+    anim_controller.play_anim(MOLE_HIDE_ANIM);
+
+    commands.spawn((
+        Sprite::from_atlas_image(
+            texture.clone(),
+            TextureAtlas {
+                layout: texture_atlas_layout.clone(),
+                index: 0,
+            },
+        ),
+        mole_start,
+        anim_controller,
+        Mole::new(key)
+    ));    
+
+    let font_pos = Vec3::new(pos.x - 22.0, pos.y + 18.0, 2.0);
+    let key_string = key_code_to_string(key);
+    commands.spawn((
+        Text2d::new(key_string),
+        text_font
+            .clone()
+            .with_font_smoothing(FontSmoothing::None),
+        Transform::from_translation(font_pos),
+        TextColor(Color::linear_rgb(0.1, 0.1, 0.1))
+    ));
+
 }
 
 /// The sprite is animated by changing its translation depending on the time that has passed since
 /// the last frame.
-fn sprite_movement(time: Res<Time>, 
-    keys: Res<ButtonInput<KeyCode>>,
-    mut sprite_position: Query<(&mut Direction, &mut Transform)>)
+// fn sprite_movement(time: Res<Time>, 
+//     keys: Res<ButtonInput<KeyCode>>,
+//     mut sprite_position: Query<(&mut Direction, &mut Transform)>)
+// {
+//     for (mut logo, mut transform) in &mut sprite_position
+//     {
+//         match *logo
+//         {
+//             Direction::Up => transform.translation.y += 150. * time.delta_secs(),
+//             Direction::Down => transform.translation.y -= 150. * time.delta_secs(),
+//             Direction::Left => transform.translation.x -= 150. * time.delta_secs(),
+//             Direction::Right => transform.translation.x += 150. * time.delta_secs(),
+//             Direction::None => {},
+//         }
+
+//         *logo = Direction::None;
+
+//         if keys.pressed(KeyCode::ArrowUp) 
+//         {
+//             *logo = Direction::Up;
+//         } 
+//         else if keys.pressed(KeyCode::ArrowDown) 
+//         {
+//             *logo = Direction::Down;
+//         }
+//         else if keys.pressed(KeyCode::ArrowLeft) 
+//         {
+//             *logo = Direction::Left;
+//         }
+//         else if keys.pressed(KeyCode::ArrowRight) 
+//         {
+//             *logo = Direction::Right;
+//         }
+//     }
+// }
+
+fn key_code_to_string(key_code: KeyCode) -> String
 {
-    for (mut logo, mut transform) in &mut sprite_position
+    match key_code
     {
-        match *logo
-        {
-            Direction::Up => transform.translation.y += 150. * time.delta_secs(),
-            Direction::Down => transform.translation.y -= 150. * time.delta_secs(),
-            Direction::Left => transform.translation.x -= 150. * time.delta_secs(),
-            Direction::Right => transform.translation.x += 150. * time.delta_secs(),
-            Direction::None => {},
-        }
-
-        *logo = Direction::None;
-
-        if keys.pressed(KeyCode::ArrowUp) 
-        {
-            *logo = Direction::Up;
-        } 
-        else if keys.pressed(KeyCode::ArrowDown) 
-        {
-            *logo = Direction::Down;
-        }
-        else if keys.pressed(KeyCode::ArrowLeft) 
-        {
-            *logo = Direction::Left;
-        }
-        else if keys.pressed(KeyCode::ArrowRight) 
-        {
-            *logo = Direction::Right;
-        }
+        KeyCode::KeyA => String::from("A"),
+        KeyCode::KeyB => String::from("B"),
+        KeyCode::KeyC => String::from("C"),
+        KeyCode::KeyD => String::from("D"),
+        KeyCode::KeyE => String::from("E"),
+        KeyCode::KeyF => String::from("F"),
+        KeyCode::KeyG => String::from("G"),
+        KeyCode::KeyH => String::from("H"),
+        KeyCode::KeyI => String::from("I"),
+        KeyCode::KeyJ => String::from("J"),
+        KeyCode::KeyK => String::from("K"),
+        KeyCode::KeyL => String::from("L"),
+        KeyCode::KeyM => String::from("M"),
+        KeyCode::KeyN => String::from("N"),
+        KeyCode::KeyO => String::from("O"),
+        KeyCode::KeyP => String::from("P"),
+        KeyCode::KeyQ => String::from("Q"),
+        KeyCode::KeyR => String::from("R"),
+        KeyCode::KeyS => String::from("S"),
+        KeyCode::KeyT => String::from("T"),
+        KeyCode::KeyU => String::from("U"),
+        KeyCode::KeyV => String::from("V"),
+        KeyCode::KeyW => String::from("W"),
+        KeyCode::KeyX => String::from("X"),
+        KeyCode::KeyY => String::from("Y"),
+        KeyCode::KeyZ => String::from("Z"),
+        _ => panic!()
     }
 }
