1cbdded9bfa4f683d77ed311548798bf2ba1eba8
August Dahlkvist <AugustDahlkvist@gmail.com>
2025-01-27 20:01:37 +0100

    Init webbuild commit

---
+++ src/menu.rs
@@ -0,0 +1,87 @@
+use crate::loading::TextureAssets;
+use crate::GameState;
+use bevy::prelude::*;
+
+pub struct MenuPlugin;
+
+/// This plugin is responsible for the game menu (containing only one button...)
+/// The menu is only drawn during the State `GameState::Menu` and is removed when that state is exited
+impl Plugin for MenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::Menu), setup_menu)
+            .add_systems(Update, click_play_button.run_if(in_state(GameState::Menu)))
+            .add_systems(OnExit(GameState::Menu), cleanup_menu);
+    }
+}
+
+#[derive(Component)]
+struct ButtonColors {
+    normal: Color,
+    hovered: Color,
+}
+
+impl Default for ButtonColors {
+    fn default() -> Self {
+        ButtonColors {
+            normal: Color::linear_rgb(0.15, 0.15, 0.15),
+            hovered: Color::linear_rgb(0.25, 0.25, 0.25),
+        }
+    }
+}
+
+#[derive(Component)]
+struct Menu;
+
+fn setup_menu(mut commands: Commands, textures: Res<TextureAssets>) {
+    info!("menu");
+    commands.spawn((Camera2d, Msaa::Off));
+    
+    // Game BG
+    commands.spawn(Sprite::from_image(textures.game_bg.clone()));
+}
+
+#[derive(Component)]
+struct ChangeState(GameState);
+
+#[derive(Component)]
+struct OpenLink(&'static str);
+
+fn click_play_button(
+    mut next_state: ResMut<NextState<GameState>>,
+    mut interaction_query: Query<
+        (
+            &Interaction,
+            &mut BackgroundColor,
+            &ButtonColors,
+            Option<&ChangeState>,
+            Option<&OpenLink>,
+        ),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    for (interaction, mut color, button_colors, change_state, open_link) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                if let Some(state) = change_state {
+                    next_state.set(state.0.clone());
+                } else if let Some(link) = open_link {
+                    if let Err(error) = webbrowser::open(link.0) {
+                        warn!("Failed to open link {error:?}");
+                    }
+                }
+            }
+            Interaction::Hovered => {
+                *color = button_colors.hovered.into();
+            }
+            Interaction::None => {
+                *color = button_colors.normal.into();
+            }
+        }
+    }
+}
+
+fn cleanup_menu(mut commands: Commands, menu: Query<Entity, With<Menu>>) {
+    for entity in menu.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
